efabaf22e40665f22154b132359d0a29a723aa33
NonSwag <mrminecraft00@gmail.com#>
2022-02-03 14:38:32 +0100

    fixed NMSItemHelper reflection bug

--- v1_16_R3/src/main/java/net/nonswag/tnl/mappings/v1_16_R3/api/item/NMSItemHelper.java
+++ v1_16_R3/src/main/java/net/nonswag/tnl/mappings/v1_16_R3/api/item/NMSItemHelper.java
@@ -12,12 +12,12 @@ public class NMSItemHelper extends ItemHelper {
     @Override
     public void setMaxStackSize(@Nonnull Material material, int maxStackSize) {
         Reflection.setField(material, "maxStack", maxStackSize);
-        Reflection.setField(Item.getById(material.ordinal()), "maxStackSize", maxStackSize);
+        Reflection.setField(Item.getById(material.ordinal()), Item.class, "maxStackSize", maxStackSize);
     }
 
     @Override
     public void setDurability(@Nonnull Material material, int durability) {
         Reflection.setField(material, "durability", durability);
-        Reflection.setField(Item.getById(material.ordinal()), "durability", durability);
+        Reflection.setField(Item.getById(material.ordinal()), Item.class, "durability", durability);
     }
 }
