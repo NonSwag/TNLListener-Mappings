81ba59910b9546dc90f229bf4940a71029a750d1
NonSwag <mrminecraft00@gmail.com#>
2022-07-15 21:17:22 +0200

    removed unnecessary method and made getCompostableItems immutable

--- v1_16_R3/src/main/java/net/nonswag/tnl/mappings/v1_16_R3/api/item/NMSItemHelper.java
+++ v1_16_R3/src/main/java/net/nonswag/tnl/mappings/v1_16_R3/api/item/NMSItemHelper.java
@@ -1,10 +1,10 @@
 package net.nonswag.tnl.mappings.v1_16_R3.api.item;
 
+import com.google.common.collect.ImmutableMap;
 import lombok.Getter;
 import net.minecraft.server.v1_16_R3.BlockComposter;
 import net.minecraft.server.v1_16_R3.IMaterial;
 import net.minecraft.server.v1_16_R3.Item;
-import net.nonswag.tnl.core.api.object.IntelligentMap;
 import net.nonswag.tnl.core.api.reflection.Reflection;
 import net.nonswag.tnl.listener.api.item.ItemHelper;
 import org.bukkit.Material;
@@ -19,7 +19,7 @@ import java.util.Map;
 public class NMSItemHelper extends ItemHelper {
 
     @Nullable
-    private HashMap<Material, Float> compostableItems = null;
+    private Map<Material, Float> compostableItems = null;
 
     @Override
     public void setMaxStackSize(@Nonnull Material material, int maxStackSize) {
@@ -33,26 +33,13 @@ public class NMSItemHelper extends ItemHelper {
         Reflection.setField(Item.getById(material.ordinal()), Item.class, "durability", durability);
     }
 
-    @Override
-    public void resetCompostableItems() {
-        getCompostableItems().clear();
-        compostableItems = null;
-        BlockComposter.c();
-    }
-
     @Nonnull
     @Override
-    public HashMap<Material, Float> getCompostableItems() {
+    public Map<Material, Float> getCompostableItems() {
         if (compostableItems != null) return compostableItems;
-        HashMap<Material, Float> items = new HashMap<>();
-        Map<IMaterial, Float> map = Reflection.<Map<IMaterial, Float>>getStaticField(BlockComposter.class, "b").getOrDefault(new HashMap<>());
+        ImmutableMap.Builder<Material, Float> items = ImmutableMap.builder();
+        var map = Reflection.<Map<IMaterial, Float>>getStaticField(BlockComposter.class, "b").getOrDefault(new HashMap<>());
         map.forEach((item, weight) -> items.put(CraftMagicNumbers.getMaterial(item.getItem()), weight));
-        return compostableItems = new IntelligentMap<>(items) {
-            @Override
-            public void update() {
-                map.clear();
-                forEach((item, weight) -> map.put(CraftMagicNumbers.getItem(item), weight));
-            }
-        };
+        return compostableItems = items.build();
     }
 }
